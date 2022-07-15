f186af352c1a39c52c5b57700bbe648854b73ceb
Joey Sanchez <joeysanchez@joeys-mbp.lan>
2022-07-15 15:22:35 -0700

    new

---
+++ public/rec2.js
@@ -0,0 +1,121 @@
+const centerBody = document.querySelector('.center-body');
+//const purge = document.querySelector('.center-body');
+
+
+
+//Initilize map and tiles:
+//Create map
+const mymap = L.map('issMap').setView([0, 0], 0);
+//Copyright
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+//Tile URL
+const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+//Creates tiles and add copyright
+const tiles = L.tileLayer(tileURL, {attribution});
+//Adds created tiles to map
+tiles.addTo(mymap);
+
+
+
+
+
+
+getData();
+async function getData() {
+    //centerTop.removeChild(purge);
+    const response = await fetch('/api');
+    const val = await response.json();
+    //console.log(val)
+    //removeAllChildNodes(centerBody);
+    for (item of val) {
+
+        const marker = L.marker([item.lat,item.lon]).addTo(mymap);
+
+
+
+
+
+        try {
+            const txt =`Weather in ${item.city_name}: ${item.weather}. The temperature is: ${item.temp}° F.
+            Lat: ${item.lat}°, 
+            Long: ${item.lon}°
+            The concentration of particulate matter (${item.air.parameter}) 
+            is ${item.air.value} ${item.air.unit}.
+            Last read on 
+            ${item.air.lastUpdated}.`
+            marker.bindPopup(txt);
+        } catch (error) {
+            const txt =`Weather in ${item.city_name}: ${item.weather}. The temperature is: ${item.temp}° F.
+            Lat: ${item.lat}°, 
+            Long: ${item.lon}°
+            No air quality reading available in this area :(`
+            marker.bindPopup(txt);
+
+        }
+
+
+
+
+
+        //const root = document.createElement('div');
+        //root.setAttribute("class", "data")
+
+        //const name = document.createElement('div');
+        //name.setAttribute("style", "margin-top: 40px");
+
+        //const geo = document.createElement('div');
+        //const lati = document.createElement('div');
+        //const long = document.createElement('div');
+
+        //const timeDate = document.createElement('div');
+        //const date = document.createElement('div');
+        //const time = document.createElement('div');
+
+
+        //Image sizing
+        //const image = document.createElement('img');
+        //image.setAttribute("style", "height: 180px; margin-bottom: 40px; margin-top: 10px;");
+
+        //const newLine = document.createElement('p');
+        //const breakl = document.createElement('br');
+
+        //console.log(item);
+        //name.textContent = "Location: "+ item.city_name;
+
+        //lati.textContent = "Latitude: " +item.lat + "°";
+        //long.textContent = "Longitude: "+item.lon + "°";
+        //geo.append(lati);
+        //geo.append(long);
+
+        //console.log(item.timestamp)
+
+        //const dateString = new Date(item.timestamp).toLocaleString();
+
+        //console.log(dateString);
+        //dateTime = dateString.split(", ");
+        //console.log(timeDate);
+        //date.textContent = "Date: " + dateTime[0];
+        //time.textContent = "Time: " + dateTime[1];
+        //timeDate.append(time);
+        //timeDate.append(date);
+        //timeDate.setAttribute("style", "margin-bottom: 40px");
+
+        //image location
+        //image.src = item.image64;
+
+        //root.append(name, geo, timeDate);  //(name, geo, timeDate, image);
+        //console.log(root);
+        //centerBody.append(root);
+    //console.log(val);
+    }
+}
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+//setInterval(removeAllChildNodes(centerTop), 4999);
+
+
+//Reload Website by calling data request
+//setInterval(getData, 1000);
---
+++ rec.js
@@ -0,0 +1,229 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mysql = require('mysql');
+//const db = require('./config/db');
+const port = process.env.PORT || 3000;
+
+
+
+
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+
+
+let connection = mysql.createConnection({
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'bc7ada4f2cece9',
+    password: '084c2219',
+    database: 'heroku_e723bcfa51ec52b',
+    multipleStatements: true,
+    insecureAuth : true
+});
+
+
+//let connection;
+
+function handleDisconnect() {
+    connection = mysql.createConnection({
+        host: 'us-cdbr-east-06.cleardb.net',
+        user: 'bc7ada4f2cece9',
+        password: '084c2219',
+        database: 'heroku_e723bcfa51ec52b',
+        multipleStatements: true,
+        insecureAuth : true
+    });
+                                                    // Recreate the connection, since
+                                                    // the old one cannot be reused.
+  
+    connection.connect(function(err) {              // The server is either down
+      if(err) {                                     // or restarting (takes a while sometimes).
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      }                                     // to avoid a hot loop, and to allow our node script to
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        handleDisconnect();                         // lost due to either server restart, or a
+      } else {                                      // connnection idle timeout (the wait_timeout
+        throw err;                                  // server variable configures this)
+      }
+    });
+}
+  
+handleDisconnect();
+
+/*= mysql.createConnection({
+    host: 'us-cdbr-east-06.cleardb.net',
+    user: 'bc7ada4f2cece9',
+    password: '084c2219',
+    database: 'heroku_e723bcfa51ec52b',
+    multipleStatements: true
+});
+
+
+connection.connect(function(err) {
+    if(err)throw err;
+    else {
+        console.log("Connection Successful");
+    }
+})*/
+
+
+
+app.set('view engine', 'ejs');
+
+console.log("START");
+
+
+
+
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/rec.html');
+    //res.render("Hello World");
+});
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port);
+console.log(`listening on port ${port}`);
+
+
+
+
+
+
+
+
+/*
+app.post('/api', (request, response) => {
+    let data = request.body;
+    try{
+            
+    var sql = "SET @id = ?;SET @lat = ?;SET @lon = ?;SET @city_name = ?;SET @weather = ?;SET @temp = ?; \
+    CALL dataAddOrEdit(@id,@lat,@lon,@city_name,@weather,@temp);";
+    connection.query(sql, [data.id, data.lat, data.lon, data.city_name, data.weather, data.temp], (error, rows, fields) => {
+        if (error) {
+            //console.log(error);
+            //connection.end();
+        }
+        if (!error) {
+        console.log(rows);
+        rows.forEach(element => {
+        if(element.constructor == Array);
+        })
+        console.log(request.body);
+        //const data = request.body;
+        const timestamp = Date.now();
+        data.timestamp = timestamp;
+        response.json(data);
+        console.log('Success');
+        //connection.end();
+        }    
+    })
+} catch (error) {
+    var sql = "SET @id = ?;SET @lat = ?;SET @lon = ?;SET @city_name = ?;SET @weather = ?;SET @temp = ?; \
+    CALL dataAddOrEdit(@id,@lat,@lon,@city_name,@weather,@temp);";
+    connection.query(sql, [data.id, data.lat, data.lon, data.city_name, data.weather, data.temp], (error, rows, fields) => {
+        if (error) {
+            //console.log(error);
+            //connection.end();
+        }
+        if (!error) {
+        console.log(rows);
+        rows.forEach(element => {
+        if(element.constructor == Array);
+        })
+        console.log(request.body);
+        //const data = request.body;
+        const timestamp = Date.now();
+        data.timestamp = timestamp;
+        response.json(data);
+        console.log('Success');
+        //connection.end();
+        }    
+    })
+}
+});
+
+
+
+
+/*
+connection.connect(function(err){   
+    if(err) throw err;
+    console.log("database connected"); //WHERE id = "1"
+    connection.query('SELECT * FROM data WHERE id = "1"', (error, rows) => {
+        if (error) {
+            console.log('error');
+            connection.end();
+        }
+        if (!error) {
+            console.log('Success');
+
+            //for(let i = 0; i < rows.length(); i++) {
+                //console.log(rows);
+            //}
+            //console.log(array);
+            let id = rows[0].id
+            console.log(id);
+            connection.end();
+            //console.log(rows.getString("city_name"));
+
+        }  
+    })
+});
+
+
+
+
+//insert
+/*
+connection.connect(function(err){   
+    if(err) throw err;
+    let data = {
+        id: 5, 
+        lat: 31.230416,
+        lon: 121.473701,
+        city_name: 'Shanghai',
+        weather: 'Broken clouds',
+        temp: 92
+    }
+    console.log("database connected");
+    var sql = "SET @id = ?;SET @lat = ?;SET @lon = ?;SET @city_name = ?;SET @weather = ?;SET @temp = ?; \
+    CALL dataAddOrEdit(@id,@lat,@lon,@city_name,@weather,@temp);";
+    connection.query(sql, [data.id, data.lat, data.lon, data.city_name, data.weather, data.temp], (error, rows, fields) => {
+        if (error) {
+            console.log(error);
+            connection.end();
+        }
+        if (!error) {
+            console.log(rows);
+            rows.forEach(element => {
+                if(element.constructor == Array);
+
+            })
+            console.log('Success');
+            connection.end();
+
+        }  
+    })
+});
+
+*/
